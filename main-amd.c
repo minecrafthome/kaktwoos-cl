f2a4ce5162b06249f46f76e99ee78b818809ef6e
BoySanic <boysonicrevived@gmail.com>
2020-12-12 18:40:17 -0500

    fix(kaktwoos): fixed how opt is assigned

--- main-amd.c
+++ main-amd.c
@@ -187,8 +187,9 @@ boinc_set_min_checkpoint_period(30);
             &kernel_length,
             &err);
     check(err, "clCreateProgramWithSource ");
-    char* opt = "-D FLOOR_LEVEL=" + floor_level;
-    err = clBuildProgram(program, 1, &device_ids, options, NULL, NULL);
+    char* opt = malloc(strlen("-D FLOOR_LEVEL=" + floor_level));
+    strcpy(opt, "-D FLOOR_LEVEL=" + floor_level);
+    err = clBuildProgram(program, 1, &device_ids, opt, NULL, NULL);
 
     if (err != CL_SUCCESS) {
         size_t len;
--- main-intel.c
+++ main-intel.c
@@ -187,7 +187,8 @@ boinc_set_min_checkpoint_period(30);
             &kernel_length,
             &err);
     check(err, "clCreateProgramWithSource ");
-    char* opt = "-D FLOOR_LEVEL=" + floor_level;
+    char* opt = malloc(strlen("-D FLOOR_LEVEL=" + floor_level));
+    strcpy(opt, "-D FLOOR_LEVEL=" + floor_level);
     err = clBuildProgram(program, 1, &device_ids, opt, NULL, NULL);
 
     if (err != CL_SUCCESS) {
